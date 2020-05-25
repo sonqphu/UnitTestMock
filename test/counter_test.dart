8c4d8b595a8bdc381132ae599f5346d38eeaf3a2
sonqphu <sonqphu@gmail.com>
2020-05-25 10:44:29 +0700

    upgrade counter_test.dart

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -3,11 +3,25 @@ import 'package:test/test.dart';
 import 'package:UnitTestMock/counter.dart';
 
 void main() {
-  test('Counter value should be incremented', () {
-    final counter = Counter();
+  group('Counter', () {
+    test('value should start at 0', () {
+      expect(Counter().value, 0);
+    });
 
-    counter.increment();
+    test('value should be incremented', () {
+      final counter = Counter();
 
-    expect(counter.value, 1);
+      counter.increment();
+
+      expect(counter.value, 1);
+    });
+
+    test('value should be decremented', () {
+      final counter = Counter();
+
+      counter.decrement();
+
+      expect(counter.value, -1);
+    });
   });
 }
